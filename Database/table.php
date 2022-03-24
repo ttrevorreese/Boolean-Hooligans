981ac9c7fa171249398716d1c3b7c2e8b9fc5b44
Sparx4life7xxx <sayeed77yahya@gmail.com>
2022-03-24 23:01:01 +0000

    DisplayDB

---
+++ Database/table.php
@@ -0,0 +1,54 @@
+<?php
+include("developers.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+ <div class="row">
+   <div class="col-sm-8">
+    <?php echo $deleteMsg??''; ?>
+    <div class="table-responsive">
+      <table class="table table-bordered">
+       <thead><tr><th>S.N</th>
+
+         <th>ID</th>
+         <th>Name</th>
+         <th>CountryCode</th>
+         <th>District</th>
+         <th>Population</th>
+    </thead>
+    <tbody>
+  <?php
+      if(is_array($fetchData)){      
+      $sn=1;
+      foreach($fetchData as $data){
+    ?>
+      <tr>
+      <td><?php echo $sn; ?></td>
+      <td><?php echo $data['ID']??''; ?></td>
+      <td><?php echo $data['Name']??''; ?></td>
+      <td><?php echo $data['CountryCode']??''; ?></td>
+      <td><?php echo $data['District']??''; ?></td>
+      <td><?php echo $data['Population']??''; ?></td> 
+     </tr>
+     <?php
+      $sn++;}}else{ ?>
+      <tr>
+        <td colspan="8">
+    <?php echo $fetchData; ?>
+  </td>
+    <tr>
+    <?php
+    }?>
+    </tbody>
+     </table>
+   </div>
+</div>
+</div>
+</div>
+</body>
+</html>

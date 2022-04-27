3c2049f0f5d9da3770be4e2b90ce19eecd2b6f45
Sparx4life7xxx <92436064+Sparx4life7xxx@users.noreply.github.com>
2022-04-27 09:48:46 +0100

    final

---
+++ backend_tests/citytable.php
@@ -0,0 +1,64 @@
+<?php 
+//Step1
+$db = mysqli_connect('localhost','root','','world')
+or die('Error connecting to MySQL server.');
+$query = "SELECT * FROM city Order By Population desc";
+    mysqli_query($db, $query) or die('Error querying database.');
+    //Step3
+    $result = mysqli_query($db, $query);
+    $row = mysqli_fetch_array($result);
+   
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="table.css">
+</head>
+<body> 
+	<table> 
+	<tr> 
+	  <h1>Cities Population Records</h1>
+		</tr> 
+			  <th> ID </th> 
+			  <th> Name </th> 
+			  <th> CountryCode </th> 
+			  <th> District <select name = 'District'>
+            <?php
+            while($rows=mysqli_fetch_assoc($result))
+            {
+              ?>
+              <option><?php echo $rows['District']; ?></option>
+            <?php
+            }
+            ?>
+          </select></th> 
+        <th> Population </th> 
+			  
+		</tr> 
+		
+		<?php 
+    //Step2
+    $query = "SELECT * FROM city Order By Population desc";
+    mysqli_query($db, $query) or die('Error querying database.');
+    //Step3
+    $result = mysqli_query($db, $query);
+    $row = mysqli_fetch_array($result);
+   
+    
+    while($rows=mysqli_fetch_assoc($result)) 
+    { 
+      ?> 
+      <tr> <td><?php echo $rows['ID']; ?></td> 
+      <td><?php echo $rows['Name']; ?></td> 
+      <td><?php echo $rows['CountryCode']; ?></td> 
+      <td><?php echo $rows['District']; ?></td> 
+      <td><?php echo $rows['Population']; ?></td>
+        </tr> 
+	<?php 
+               } 
+              
+          ?> 
+
+	</table> 
+	</body> 
+</html>
